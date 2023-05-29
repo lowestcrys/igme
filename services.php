cc006315b5d35eb8f7af76b9427704e12a8d0622
lowestcrys <reyescb@students.nu-baliwag.edu.ph>
2023-05-29 23:08:54 +0800

    wala

--- services.php
+++ services.php
@@ -52,6 +52,7 @@ i#next {
   right: 25px;
 }
 .image-container {
+  background-image: url(home2.png);
   height: 320px;
   max-width: 500px;
   width: 100%;
@@ -75,7 +76,7 @@ i#next {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Image Slider JavaScript | CodingNepal</title>
+    <title>IGME</title>
     <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
     <script src="script.js" defer></script>
